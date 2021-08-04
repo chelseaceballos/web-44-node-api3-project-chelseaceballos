4826632f4296c21d2cc142ee50c461d5bf527b43
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-04 11:46:23 -0700

    server is running successfully

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const userRouter = require('./users/users-router');
 const server = express();
+
+server.use(express.json()); // req.body {}
+
 
 // remember express by default cannot parse JSON in request bodies
 
